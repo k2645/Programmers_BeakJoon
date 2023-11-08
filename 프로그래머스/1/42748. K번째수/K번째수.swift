4cb485be2ad533085b6516f7c6f450918d56686d
00me <62226667+k2645@users.noreply.github.com>
2023-11-08 21:58:20 +0900

    [level 1] Title: K번째수, Time: 0.21 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
+    
+    var result: [Int] = []
+    
+    for command in commands {
+        let i = command[0] - 1
+        let j = command[1] - 1
+        let k = command[2] - 1
+        result.append(array[i...j].sorted()[k])
+    }
+    
+    return result
+}
