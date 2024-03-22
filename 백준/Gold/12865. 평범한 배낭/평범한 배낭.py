a4c09d9cbd812e731b9f02a1d6048ce083bbff71
00me <62226667+k2645@users.noreply.github.com>
2024-03-22 18:08:12 +0900

    [Gold V] Title: 평범한 배낭, Time: 744 ms, Memory: 41968 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -3,13 +3,13 @@ import sys
 N, K = map(int, sys.stdin.readline().split())
 objects = [tuple(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-dp = [[0] * (K + 1) for _ in range(N + 1)]
+dp = {0: 0}
+for i in range(N):
+    new_w, new_v = objects[i]
+    temp = {}
+    for acc_w, acc_v in dp.items():
+        if acc_w + new_w <= K and acc_v + new_v > dp.get(acc_w + new_w, 0):
+            temp[acc_w + new_w] = acc_v + new_v
+    dp.update(temp)
 
-for i in range(1, N + 1):
-    for w in range(1, K + 1):
-        weight, value = objects[i - 1]
-        if weight <= w:
-            dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weight] + value)
-        else:
-            dp[i][w] = dp[i - 1][w]
-print(dp[N][K])
+print(max(dp.values()))
